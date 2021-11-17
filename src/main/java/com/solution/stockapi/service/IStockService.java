cefda3bc4f6ab7a009c0a67f059f08f9098e57c9
Emmanuel Abajo <temiabajo@gmail.com>
2021-11-17 17:42:05 +0100

    added java docs to service layer

--- src/main/java/com/solution/stockapi/service/IStockService.java
+++ src/main/java/com/solution/stockapi/service/IStockService.java
@@ -7,10 +7,38 @@ import com.solution.stockapi.entity.Stock;
 
 public interface IStockService {
 	
+	/**
+	 * Create and persist new stock object
+	 * @param stockDto stock object creation request object. Contains name and price
+	 * @return Stock stock object stored in data store
+	 */
 	Stock create(StockDTO stockDto);
+
+	/**
+	 * Fetch all existing stock objects
+	 * @return List<StockDTO> list of stock objects
+	 */
 	List<StockDTO> getAll();
+
+	/**
+	 * Delete stock object from data store
+	 * @param id stock object identifier
+	 */
 	void delete(Integer id);
+
+	/**
+	 * Fetch stock object by identifier
+	 * @param id stock object identifier
+	 * @return StockDTO sub stock object
+	 */
 	StockDTO getByID(Integer id);
+
+	/**
+	 * Update stock object name/price
+	 * @param stockDto stock object update request object. Contains name and/or price
+	 * @param id stock object identifier
+	 * @return StockDTO sub stock object
+	 */
 	StockDTO update(StockDTO stockDto, Integer id);
 
 }
